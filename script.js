2b5967f7650cc09d0bb4e26f4784af6f60005b4e
Terry Chen <chenterry85@gmail.com>
2020-03-23 03:30:47 +0800

    update slideshow

--- script.js
+++ script.js
@@ -1,125 +1,19 @@
-// Smooth Scroll on anchor links
-(function() {
-     'use strict';
-
-    // Feature Test
-    if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
-
-        // Function to animate the scroll
-        var smoothScroll = function (anchor, duration) {
-            console.log("smoothScroll")
-
-            // Calculate how far and how fast to scroll
-            var startLocation = window.pageYOffset;
-            var endLocation = anchor.offsetTop;
-            var distance = endLocation - startLocation;
-            var increments = distance/(duration/16);
-            var stopAnimation;
-
-            // Scroll the page by an increment, and check if it's time to stop
-            var animateScroll = function () {
-                window.scrollBy(0, increments);
-                stopAnimation();
-            };
-
-            // If scrolling down
-            if ( increments >= 0 ) {
-                // Stop animation when you reach the anchor OR the bottom of the page
-                stopAnimation = function () {
-                    var travelled = window.pageYOffset;
-                    if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
-                        clearInterval(runAnimation);
-                    }
-                };
-            }
-            // If scrolling up
-            else {
-                // Stop animation when you reach the anchor OR the top of the page
-                stopAnimation = function () {
-                    var travelled = window.pageYOffset;
-                    if ( travelled <= (endLocation || 0) ) {
-                        clearInterval(runAnimation);
-                    }
-                };
-            }
-
-            // Loop the animation function
-            var runAnimation = setInterval(animateScroll, 16);
-
-        };
-
-        // Define smooth scroll links
-        var scrollToggle = document.querySelectorAll('.scroll');
-
-        // For each smooth scroll link
-        [].forEach.call(scrollToggle, function (toggle) {
-
-            // When the smooth scroll link is clicked
-            toggle.addEventListener('click', function(e) {
-
-                // Prevent the default link behavior
-                e.preventDefault();
-
-                // Get anchor link and calculate distance from the top
-                var dataID = toggle.getAttribute('href');
-                var dataTarget = document.querySelector(dataID);
-                var dataSpeed = toggle.getAttribute('data-speed');
-
-                // If the anchor exists
-                if (dataTarget) {
-                    // Scroll to the anchor
-                    smoothScroll(dataTarget, dataSpeed || 500);
-                }
-
-            }, false);
-
-        });
-
-    }
-
- })();
-
-//Smooth Scroll
-SmoothScroll({
-    // Scrolling Core
-    animationTime    : 400, // [ms]
-    stepSize         : 100, // [px]
-
-    // Acceleration
-    accelerationDelta : 50,  // 50
-    accelerationMax   : 3,   // 3
-
-    // Keyboard Settings
-    keyboardSupport   : true,  // option
-    arrowScroll       : 50,    // [px]
-
-    // Pulse (less tweakable)
-    // ratio of "tail" to "acceleration"
-    pulseAlgorithm   : true,
-    pulseScale       : 4,
-    pulseNormalize   : 1,
-
-    // Other
-    touchpadSupport   : false, // ignore touchpad by default
-    fixedBackground   : true,
-    excluded          : ''
-});
-
 // Modal Image Gallery
-function onClick(element) {
+function expandImage(element) {
   document.getElementById("img01").src = element.src;
   document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
 }
 
 // Change style of navbar on scroll
 window.onscroll = function() {myFunction()};
 function myFunction() {
-    var navbar = document.getElementById("myNavBar")
+    var navbar = document.getElementById("NavBar")
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar " + "w3-navbar " + "w3-wide" + " w3-card" + " w3-animate-top" + " w3-white";
+        navbar.className = "w3-bar " + "w3-wide" + " w3-card" + " w3-animate-top" + " w3-white";
     } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
-        console.log(navbar)
+        navbar.className = navbar.className.replace("w3-card w3-animate-top w3-white", "");
     }
 }
 
@@ -148,4 +42,28 @@ function scrollToTop(scrollDuration) {
         }
         else clearInterval(scrollInterval);
     },15);
+}
+
+//slideshow
+setTimeout(carousel, 500) // Change image every 2 seconds
+
+var slide_index = 0;
+function carousel() {
+  var x = ["url('image/Bike.jpg')","url('image/bike1.jpg')","url('image/bike7.jpg')"]
+  var slideshow = document.getElementsByClassName("slideshow")[0];
+  var dots = document.getElementsByClassName("dot");
+
+  //slideshow
+  slide_index++
+  if (slide_index > x.length) {slide_index = 1}
+  slideshow.style.backgroundImage = x[slide_index - 1]
+
+  //dot
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" dot-active", "");
+  }
+  dots[slide_index-1].className += " dot-active";
+
+
+  setTimeout(carousel, 3000) // Change image every 2 seconds
 }
